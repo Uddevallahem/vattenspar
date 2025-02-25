0d108a0bccab290dd39245c4a33015a5299baa1b
Uddevallahem <vila3631@gmail.com>
2025-02-25 13:53:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function generateHTML(data) {
                 
                 .slider-value {
                     position: absolute;
-                    margin-top: 20px;
+                    margin-top: -20px;
                     top: -25px;
                     left: 50%;
                     transform: translateX(-50%);
